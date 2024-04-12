0b51708a5447e6e0066110b17c96ba62867ecc08
Mahmod Mashlah <mashlahmahmod@gmail.com>
2024-04-12 06:52:21 +0300

    9- fix javascripts links in footer-scripts.blade

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,24 +1,65 @@
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+{{--   this line below will converted  --}}
+{{-- <script src="plugins/jquery/jquery.min.js"></script> --}}
+{{-- to --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+{{-- <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
 <!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+{{-- <script src="../../plugins/datatables/jquery.dataTables.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+
+{{-- <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}">
+</script>
+
+{{-- <script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script> --}}
+<script type="text/javascript"
+    src="{{ URL::asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+
+{{-- <script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script> --}}
+<script type="text/javascript"
+    src="{{ URL::asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+
+{{-- <script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}">
+</script>
+
+{{-- <script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}">
+</script>
+
+{{-- <script src="../../plugins/jszip/jszip.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+
+{{-- <script src="../../plugins/pdfmake/pdfmake.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+
+{{-- <script src="../../plugins/pdfmake/vfs_fonts.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+
+{{-- <script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}">
+</script>
+
+{{-- <script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}">
+</script>
+
+{{-- <script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}">
+</script>
+
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+{{-- <script src="../../dist/js/adminlte.min.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/js/adminlte.min.js') }}"></script>
+
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+{{-- <script src="../../dist/js/demo.js"></script> --}}
+<script type="text/javascript" src="{{ URL::asset('assets/js/demo.js') }}"></script>
+
 <!-- Page specific script -->
 <script>
     $(function() {
